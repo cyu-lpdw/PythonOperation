c8cf63ed0f81e2c6c7bf8a157c3020f8b7b2c50c
GLacaille <glacaille.s@gmail.com>
2023-09-07 15:00:14 +0200

    Ajout d'une fonction qui affiche le prénom quand l'url est modifiée

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/<string:prenom>")
+def hello_person(prenom):
+    return f"<p>Hello, {prenom}!</p>"
