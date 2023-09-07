2a640db84eee1673dc0bb5ec7a76a1b408267e0c
ANSUMDINE <ansumdinesaidcombo@gmail.com>
2023-09-07 21:59:06 +0200

    correction

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-@app.route('/')
-def hello():
-    return 'Hello said'
+@app.route("/<string:prenom>")
+def hello_person(prenom):
+    return f"<p>Hello, {prenom}!</p>"
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
