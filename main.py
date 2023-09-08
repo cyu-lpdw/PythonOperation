c4fe63a8e1adc865fa3f7f8a690a59ec5307c339
thierry <thierrydu92@yahoo.fr>
2023-09-08 16:37:51 +0200

    add fils

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-if __name__ == '__main__':
-    print('main')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """
+    Renders home page
+    :return: Renders the 'index.html' template (home page)
+    """
+    return render_template('index.html')
+
+
+@app.route("/<string:prenom>")
+def hello_person(prenom):
+    """
+    Renders a personalized greetings page based on the provided name.
+    :param prenom: The name of the person to greet.
+    :return: Renders the 'name.html' template (greetings page)
+    """
+    return render_template('name.html', prenom=prenom)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
