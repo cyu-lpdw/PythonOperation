9f5fe293892aa013fc5bd6b335bf12d0962f5a8a
GLacaille <glacaille.s@gmail.com>
2023-09-08 13:19:24 +0200

    Add home

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 @app.route("/<string:prenom>")
 def hello_person(prenom):
