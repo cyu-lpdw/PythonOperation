f813e0cac83383281b403adda9883f18e2221071
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2023-09-08 12:00:57 +0200

    Added template with jinja from Flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/<string:prenom>")
+def hello_person(prenom):
+    return render_template('name.html', prenom=prenom)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
