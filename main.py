c5fb5b5b653bde291f91464ec1d2e9c8e4f43468
Koundidjo <mohamed.laacherpro@gmail.com>
2023-09-08 11:09:27 +0200

    ajout de fichiers

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from jinja2 import Environment, FileSystemLoader
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route("/<string:prenom>")
+def hello_person(prenom):
+    return render_template('name.html', prenom=prenom)
+
+if __name__ == "__main__":
+    app.run(debug=True)
