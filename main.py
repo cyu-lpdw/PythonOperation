b356dc0ce67d48dbd87c94ad340f0b890cc2e337
yaokissi <ekissi442@gmail.com>
2023-09-08 15:35:00 +0200

    jajout des fichiers

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-if __name__ == '__main__':
-    print('main')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/<string:prenom>")
+def hello_person(prenom):
+    return render_template('name.html', prenom=prenom)
+
+if __name__ == "__main__":
+    app.run(debug=True)
