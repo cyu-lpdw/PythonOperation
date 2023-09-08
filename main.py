a7d0e57a0d20a16879812f16679b99663ece0a9a
GLacaille <glacaille.s@gmail.com>
2023-09-08 09:36:42 +0200

    Ajout d'une méthode de template poAjout d'une méthode de template pour afficher son Sur la branche devel/gustave

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,4 +8,7 @@ def hello_world():
 
 @app.route("/<string:prenom>")
 def hello_person(prenom):
-    return f"<p>Hello, {prenom}!</p>"
+    return render_template('name.html', prenom=prenom)
+
+if __name__ == "__main__":
+    app.run(debug=True)
